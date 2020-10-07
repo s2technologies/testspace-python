5836e2b23220cc604ae913c2f9ba412626653192
jaschultz <jschultz8960@yahoo.com>
2020-10-07 12:48:13 -0700

    update based on

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # testspace-python
-Library to interact with Testspace api.
+
+[![Code style: black][black-badge]][black-link]
+
+Library to use Testspace API and client.
+
+## Installation
+
+Install from PyPi
+
+```console
+$ pip install testspace-python
+```
+
+or locally:
+
+```
+$ pip install -e .[code_style,testing]
+```
+
+## Testing
+
+Enter created folder then run tests:
+
+```
+$ flake8 .
+$ black .
+$ pytest
+```
+
+To use pre-commit:
+
+```
+$ git add *
+# to apply to staged files
+$ pre-commit run
+# restage if changes
+$ git add *
+# to run on commits
+$ pre-commit install
+$ git commit -m 'Initial commit'
+```
+
+[black-badge]: https://img.shields.io/badge/code%20style-black-000000.svg
+[black-link]: https://github.com/ambv/black
+
+(package originally created by [python-pkg-cookiecutter](https://github.com/executablebooks/python-pkg-cookiecutter))
--- setup.py
+++ setup.py
@@ -1,15 +1,52 @@
+#!/usr/bin/env python
+"""The setup script."""
 from setuptools import setup, find_packages
 
+with open("testspace/__init__.py", "r") as handle:
+    for line in handle:
+        if "__version__" in line:
+            version = line.split(" = ")[-1].strip('"')
+            break
+
+with open("./README.md", "r") as handle:
+    readme_text = handle.read()
+
+with open("./requirements.txt", "r") as handle:
+    requirements = [lr.strip() for lr in handle.read().splitlines() if lr.strip()]
+
+with open("./requirements_dev.txt", "r") as handle:
+    requirements_dev = [lv.strip() for lv in handle.read().splitlines() if lv.strip()]
+
+with open("./requirements_doc.txt", "r") as handle:
+    requirements_doc = [ld.strip() for ld in handle.read().splitlines() if ld.strip()]
+
 setup(
-    name='testspace-python',
-    version='',
-    packages=find_packages(include=['testspace', 'testspace.*']),
-    url='',
-    license="MIT license",
+    name="testspace-python",
+    version="0.1.1",
+    description="Library to use Testspace API and client.",
+    long_description=readme_text,
+    long_description_content_type="text/markdown",
     author="Jeffrey Schultz",
-    author_email='jeffs@s2technologies.com',
-    description="Module for interacting with Testspace Server",
-    install_requires=[
-        'requests',
-    ]
+    author_email="jschultz8960@yahoo.com",
+    url=(
+        "https://github.com/"
+        "s2technologies/testspace-python"
+    ),
+    license="MIT",
+    packages=find_packages(),
+    # include anything specified in Manifest.in
+    include_package_data=True,
+    install_requires=requirements,
+    extras_require={
+        "code_style": ["flake8<3.8.0,>=3.7.0", "black", "pre-commit==1.17.0"],
+        "testing": requirements_dev,
+        "docs": requirements_doc,
+    },
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="",
 )
