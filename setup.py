b0746fe91e10349c89f089555d0842e8a5590e48
Ivailo Petrov <5092875+ivailop@users.noreply.github.com>
2024-04-11 13:30:17 -0700

    set complient setup version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='testspace-python',
-    version='',
+    version='0.1.5',
     packages=find_packages(include=['testspace', 'testspace.*']),
     url='',
     license="MIT license",
